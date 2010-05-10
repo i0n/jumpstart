121ae8d7de5bc7e2214e0097ba9923ae592a309f
i0n <ianalexanderwood@gmail.com>
2010-05-10 03:10:35 +0100

    moved setup commands to yml file

--- jumpstart_templates/rails_3/jumpstart_config/rails_3.rb
+++ jumpstart_templates/rails_3/jumpstart_config/rails_3.rb
@@ -1,24 +1,10 @@
 
 
-system "haml --rails ."
-
-# create databases
-system "rake db:create:all"
-
 # Change Production environment config for use with remote server
 FileUtils.append_to_end_of_file('  socket: /var/run/mysqld/mysqld.sock', "#{@install_path}/#{@project_name}/config/database.yml", true)
 
 # config for local OSX Nginx environment
 FileUtils.nginx_auto_config("#{@template_path}/config/nginx.local.conf", '/usr/local/nginx/conf/nginx.conf', "#{@project_name}")
-
-# Appending URI to /etc/hosts to complete local OSX Nginx config
-system "sudo chmod 777 /etc/hosts"
-system "echo '\n127.0.0.1 #{app_name}.local'>>/etc/hosts"
-
-################################################################### CAPISTRANO CONFIGURATION #############################################################
-
-# Set application up for deployment with Capistrano
-system "capify!"
 
 # Read Capistrano template and replace remote_server and app_name variables with the values entered during app creation
 # cap_txt = IO.read("#{rails_templates_path}/rails/config/deploy.rb")
@@ -27,13 +13,6 @@ system "capify!"
 # file 'config/deploy.rb' do
 #   cap_txt
 # end
-
-# Initialise Git repository
-system "git init"
-
-# Add all changes to the Git repository and then commit those changes.
-system "git add ."
-system "git commit -v -a -m Initial commit"
 
 ################################################################### REMOTE DEPLOYMENT ##############################################################
 
