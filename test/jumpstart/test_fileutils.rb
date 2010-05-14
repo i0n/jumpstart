8bb215a189904d14eb334af26ead5308c4bc765e
i0n <ianalexanderwood@gmail.com>
2010-05-14 21:08:56 +0100

    setup structure for testing JumpStart::FileUtils class methods

--- test/jumpstart/test_fileutils.rb
+++ test/jumpstart/test_fileutils.rb
@@ -2,7 +2,43 @@ require 'helper'
 
 class TestJumpstartFileUtils < Test::Unit::TestCase
   
-  should "be able to run jumpstart" do
+  context "Testing JumpStart::FileUtils class methods.\n" do
+    
+    context "Testing JumpStart::FileUtils.append_after_line class method" do
+      
+    end
+
+    context "Testing JumpStart::FileUtils.append_to_end_of_file class method" do
+      
+    end
+
+    context "Testing JumpStart::FileUtils.insert_text_at_line_number class method" do
+      
+    end
+
+    context "Testing JumpStart::FileUtils.remove_files class method" do
+      
+    end
+
+    context "Testing JumpStart::FileUtils.config_nginx class method" do
+      
+    end
+
+    context "Testing JumpStart::FileUtils.config_etc_hosts class method" do
+      
+    end
+
+    context "Testing JumpStart::FileUtils.config_capistrano class method" do
+      
+    end
+
+    context "Testing JumpStart::FileUtils.make_bare_git_repo class method" do
+      
+    end
+
+    context "Testing JumpStart::FileUtils.check_source_type class method" do
+      
+    end
     
   end
   
