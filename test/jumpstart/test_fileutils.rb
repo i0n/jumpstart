1074d19c33148c3983e94b3d35e59dca132fbb61
i0n <ianalexanderwood@gmail.com>
2010-05-15 02:51:52 +0100

    written append after line tests

--- test/jumpstart/test_fileutils.rb
+++ test/jumpstart/test_fileutils.rb
@@ -22,27 +22,17 @@ class TestJumpstartFileUtils < Test::Unit::TestCase
       file = IO.readlines("#{JumpStart::ROOT_PATH}/test/test_jumpstart_templates/test_fileutils/append_after_line_test.txt")
       assert_equal(4, (file.find_index("Inserted by append_after_line method test.\n").to_i + 1))
     end
-    
+        
     should "fail because new line is not specified." do
-      FileUtils.append_after_line("#{JumpStart::ROOT_PATH}/test/test_jumpstart_templates/test_fileutils/append_after_line_test.txt", "Line from check_source_type.txt. Line number: 3")
+      assert_raises(ArgumentError) {FileUtils.append_after_line("#{JumpStart::ROOT_PATH}/test/test_jumpstart_templates/test_fileutils/append_after_line_test.txt", "Line from check_source_type.txt. Line number: 3")}
+    end
+    
+    should "fail because the target file does not exist" do
+      assert_raises(Errno::ENOENT) {FileUtils.append_after_line("#{JumpStart::ROOT_PATH}/test/test_jumpstart_templates/test_fileutils/this_file_does_not_exist.txt", "Line from check_source_type.txt. Line number: 3", "Inserted by append_after_line method test.")}
     end
     
   end
   
-  # def append_after_line(target_file, target_line, new_line)
-  #   file = IO.readlines(target_file)
-  #   new_file = file.dup
-  #   file.each do |line|
-  #     if line.chomp == target_line.chomp && new_line != nil
-  #       new_file.insert((file.find_index(line) + 1), new_line)
-  #       new_line = nil
-  #     end
-  #   end
-  #   File.open(target_file, "w") do |x|
-  #     x.puts new_file
-  #   end
-  # end
-
   context "Testing JumpStart::FileUtils.append_to_end_of_file class method" do
     
   end
