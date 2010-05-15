4534c32c900e561ff2c0a1f25da007bf31a041f3
i0n <ianalexanderwood@gmail.com>
2010-05-15 03:47:23 +0100

    wrote tests for append_to_end_of_file method

--- test/jumpstart/test_fileutils.rb
+++ test/jumpstart/test_fileutils.rb
@@ -5,7 +5,7 @@ class TestJumpstartFileUtils < Test::Unit::TestCase
   context "Testing JumpStart::FileUtils.append_after_line class method" do
     
     setup do
-      FileUtils::remove_lines("#{JumpStart::ROOT_PATH}/test/test_jumpstart_templates/test_fileutils/append_after_line_test.txt", "Inserted by append_after_line method test.")
+      FileUtils.remove_lines("#{JumpStart::ROOT_PATH}/test/test_jumpstart_templates/test_fileutils/append_after_line_test.txt", "Inserted by append_after_line method test.")
     end
     
     should "insert specified line at line number 4 of target file" do
@@ -27,15 +27,49 @@ class TestJumpstartFileUtils < Test::Unit::TestCase
   context "Testing JumpStart::FileUtils.append_to_end_of_file class method" do
     
     setup do
-      FileUtils::remove_lines("#{JumpStart::ROOT_PATH}/test/test_jumpstart_templates/test_fileutils/append_to_end_of_file_test.txt", "TEST LINE INSERTED")
+      @file_path = "#{JumpStart::ROOT_PATH}/test/test_jumpstart_templates/test_fileutils/append_to_end_of_file_test.txt"
+      FileUtils.remove_lines(@file_path, "TEST LINE INSERTED")
+      FileUtils.remove_lines(@file_path, "TEST LINE INSERTED FROM FILE")
     end
     
-    should "Add the string passed in the method call to the specified file" do
-      FileUtils.append_to_end_of_file("TEST LINE INSERTED", "#{JumpStart::ROOT_PATH}/test/test_jumpstart_templates/test_fileutils/append_to_end_of_file_test.txt")
-      file = IO.readlines("#{JumpStart::ROOT_PATH}/test/test_jumpstart_templates/test_fileutils/append_to_end_of_file_test.txt")
+    should "add the string passed in the method call to the specified file" do
+      FileUtils.append_to_end_of_file("TEST LINE INSERTED", @file_path)
+      file = IO.readlines(@file_path)
       assert_equal(6, (file.find_index("TEST LINE INSERTED\n").to_i + 1))
     end
+
+    should "add the string passed in the method call to the specified file and remove the last line" do
+      # Two appended lines so that the file doesn't lose all lines over time.
+      FileUtils.append_to_end_of_file("TEST LINE INSERTED", @file_path)
+      FileUtils.append_to_end_of_file("TEST LINE INSERTED", @file_path, true)
+      file = IO.readlines(@file_path)
+      assert_equal("TEST LINE INSERTED\n", file.last)
+      assert_equal(6, file.count)
+    end
     
+    should "add the contents of the file passed in the method call to the specified file" do
+      FileUtils.append_to_end_of_file("#{JumpStart::ROOT_PATH}/test/test_jumpstart_templates/test_fileutils/append_to_end_of_file_source.txt", @file_path)
+      file = IO.readlines(@file_path)
+      assert_equal(6, (file.find_index("TEST LINE INSERTED FROM FILE\n").to_i + 1))
+    end
+
+    should "add the contents of the file passed in the method call to the specified file and remove the last line" do
+      # Two appended lines so that the file doesn't lose all lines over time.
+      FileUtils.append_to_end_of_file("#{JumpStart::ROOT_PATH}/test/test_jumpstart_templates/test_fileutils/append_to_end_of_file_source.txt", @file_path)
+      FileUtils.append_to_end_of_file("#{JumpStart::ROOT_PATH}/test/test_jumpstart_templates/test_fileutils/append_to_end_of_file_source.txt", @file_path, true)
+      file = IO.readlines(@file_path)
+      assert_equal("TEST LINE INSERTED FROM FILE\n", file.last)
+      assert_equal(6, file.count)
+    end
+    
+    should "fail because no target file is specified" do
+      assert_raises(ArgumentError) {FileUtils.append_to_end_of_file("TEST LINE INSERTED")}
+    end
+    
+    should "fail because target file does not exist" do
+      assert_raises(Errno::ENOENT) {FileUtils.append_to_end_of_file("TEST LINE INSERTED", "#{JumpStart::ROOT_PATH}/test/test_jumpstart_templates/does_not_exist/file.txt")}
+    end
+        
   end
 
   context "Testing JumpStart::FileUtils.insert_text_at_line_number class method" do
@@ -43,6 +77,10 @@ class TestJumpstartFileUtils < Test::Unit::TestCase
   end
 
   context "Testing JumpStart::FileUtils.remove_files class method" do
+    
+  end
+  
+  context "Testing JumpStart::FileUtils.remove_lines class method" do
     
   end
 
