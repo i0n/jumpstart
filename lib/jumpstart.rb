b2661ec458907f2ca6e765abdf4d20ebf7cbdd59
i0n <ianalexanderwood@gmail.com>
2010-05-18 20:30:50 +0100

    decided to use correct spelling for methods and tidied up slightly

--- lib/jumpstart.rb
+++ lib/jumpstart.rb
@@ -25,14 +25,15 @@ end
 
 class String
     
-  def red; colorize(self, "\e[1m\e[31m"); end
-  def green; colorize(self, "\e[32m"); end
-  def green_bold; colorize(self, "\e[1m\e[32m"); end
-  def yellow; colorize(self, "\e[1m\e[33m"); end
-  def blue; colorize(self, "\e[34m"); end
-  def blue_bold; colorize(self, "\e[1m\e[34m"); end
-  def purple; colorize(self, "\e[1m\e[35m"); end
-  def colorize(text, color_code)  "#{color_code}#{text}\e[0m" end
+  def red;        colourise(self, "\e[1m\e[31m"); end
+  def green;      colourise(self, "\e[32m"); end
+  def green_bold; colourise(self, "\e[1m\e[32m"); end
+  def yellow;     colourise(self, "\e[1m\e[33m"); end
+  def blue;       colourise(self, "\e[34m"); end
+  def blue_bold;  colourise(self, "\e[1m\e[34m"); end
+  def purple;     colourise(self, "\e[1m\e[35m"); end
+  
+  def colourise(text, colour_code)  "#{colour_code}#{text}\e[0m" end
   
   # Codes for changing output text:
 
