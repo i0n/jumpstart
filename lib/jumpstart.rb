7575cf57ba256e10a2f9ff9e64ad84d1eb79ec94
i0n <ianalexanderwood@gmail.com>
2010-05-26 10:16:02 +0100

    moved String extension methods into their own module JumpStart::StringTools required in jumpstart and then included in String class

--- lib/jumpstart.rb
+++ lib/jumpstart.rb
@@ -19,6 +19,7 @@ module JumpStart
   
   require 'jumpstart/base'
   require 'jumpstart/filetools'
+  require 'jumpstart/stringtools'
 
   # The path to the jumpstart templates directory. 
   # Set as a module instance variable.
@@ -41,45 +42,6 @@ module FileUtils
   end
 end
 
-# TODO Move String extension methods into their own module and then include in String class.
 class String
-    
-  def red;          colourise(self, "\e[31m"); end
-  def red_bold;     colourise(self, "\e[1m\e[31m"); end
-  def green;        colourise(self, "\e[32m"); end
-  def green_bold;   colourise(self, "\e[1m\e[32m"); end
-  def yellow;       colourise(self, "\e[1m\e[33m"); end
-  def blue;         colourise(self, "\e[34m"); end
-  def blue_bold;    colourise(self, "\e[1m\e[34m"); end
-  def purple;       colourise(self, "\e[1m\e[35m"); end
-  
-  def colourise(text, colour_code)  "#{colour_code}#{text}\e[0m" end
-  
-  # Codes for changing output text:
-
-  # 0   Turn off all attributes
-  # 1   Set bright mode
-  # 4   Set underline mode
-  # 5   Set blink mode
-  # 7   Exchange foreground and background colors
-  # 8   Hide text (foreground colour would be the same as background)
-  # 30  Black text
-  # 31  Red text
-  # 32  Green text
-  # 33  Yellow text
-  # 34  Blue text
-  # 35  Magenta text
-  # 36  Cyan text
-  # 37  White text
-  # 39  Default text colour
-  # 40  Black background
-  # 41  Red background
-  # 42  Green background
-  # 43  Yellow background
-  # 44  Blue background
-  # 45  Magenta background
-  # 46  Cyan background
-  # 47  White background
-  # 49  Default background colour
-      
+  include JumpStart::StringTools
 end
---
+++ lib/jumpstart/stringtools.rb
@@ -0,0 +1,41 @@
+module JumpStart::StringTools
+
+  def red;          colourise(self, "\e[31m"); end
+  def red_bold;     colourise(self, "\e[1m\e[31m"); end
+  def green;        colourise(self, "\e[32m"); end
+  def green_bold;   colourise(self, "\e[1m\e[32m"); end
+  def yellow;       colourise(self, "\e[1m\e[33m"); end
+  def blue;         colourise(self, "\e[34m"); end
+  def blue_bold;    colourise(self, "\e[1m\e[34m"); end
+  def purple;       colourise(self, "\e[1m\e[35m"); end
+
+  def colourise(text, colour_code)  "#{colour_code}#{text}\e[0m" end
+
+  # Codes for changing output text:
+
+  # 0   Turn off all attributes
+  # 1   Set bright mode
+  # 4   Set underline mode
+  # 5   Set blink mode
+  # 7   Exchange foreground and background colors
+  # 8   Hide text (foreground colour would be the same as background)
+  # 30  Black text
+  # 31  Red text
+  # 32  Green text
+  # 33  Yellow text
+  # 34  Blue text
+  # 35  Magenta text
+  # 36  Cyan text
+  # 37  White text
+  # 39  Default text colour
+  # 40  Black background
+  # 41  Red background
+  # 42  Green background
+  # 43  Yellow background
+  # 44  Blue background
+  # 45  Magenta background
+  # 46  Cyan background
+  # 47  White background
+  # 49  Default background colour
+
+end
