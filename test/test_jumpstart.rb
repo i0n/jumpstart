61a14acf9a81a9882a9806d8b687ba365a68fe94
i0n <ianalexanderwood@gmail.com>
2010-05-28 14:27:57 +0100

    wrote tests for JumpStart module

--- test/test_jumpstart.rb
+++ test/test_jumpstart.rb
@@ -6,4 +6,17 @@ class TestJumpstart < Test::Unit::TestCase
     assert(File.exists?("#{JumpStart::CONFIG_PATH}/jumpstart_setup.yml"))
   end
   
+  should "be able to find jumpstart_version.yml" do
+    assert(File.exists?("#{JumpStart::CONFIG_PATH}/jumpstart_version.yml"))
+  end
+  
+  context "Test for JumpStart.version class instance method" do
+    should "return 1.1.1" do
+      JumpStart::Setup.version_major = 1
+      JumpStart::Setup.version_minor = 1
+      JumpStart::Setup.version_patch = 1
+      assert_equal "1.1.1", JumpStart.version
+    end
+  end
+  
 end
